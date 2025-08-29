27a808666bbd05666fa89ce1877b1093c7e43c3d
QianNianSiHuan <1409050584@qq.com>
2025-08-30 04:02:18 +0800

    fix:表结构优化

--- app/model/user_black.go
+++ app/model/user_black.go
@@ -4,8 +4,8 @@ type UserBlack struct {
 	BaseModel
 	UserID   uint   `json:"userId"`
 	User     User   `gorm:"foreignkey:UserID" json:"_"`
-	Type     int8   `gorm:"type:tinyint" json:"type"` //1.封禁2.风控3.异地登录
+	Type     int8   `gorm:"type:smallint" json:"type"` //1.封禁2.风控3.异地登录
 	Reason   string `gorm:"size:255"`
-	StarTime int    `gorm:"type:int(11)" json:"starTime"`
-	StopTime int    `gorm:"type:int(11)" json:"stopTime"`
+	StarTime int    `json:"starTime"`
+	StopTime int    `json:"stopTime"`
 }
